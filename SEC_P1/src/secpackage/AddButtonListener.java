793d5905274b060fe75a6c72e034b24b5a57f56d
Magic <sai.abhishek.2008@gmail.com>
2020-02-28 15:42:22 -0500

    comments to add action listener

--- SEC_P1/src/secpackage/AddButtonListener.java
+++ SEC_P1/src/secpackage/AddButtonListener.java
@@ -10,7 +10,7 @@ import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 /**
- *
+ * Action  Listener class for add button in SECP1_Form
  * @author sanidhya
  */
 public class AddButtonListener implements java.awt.event.ActionListener
